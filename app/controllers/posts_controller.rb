4f7561720357d239b6b9426818294a570dd9d967
yuuki kajiyama <luv4u.1331@gmail.com>
2021-02-05 15:29:45 +0900

    feat.:投稿確認機能追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,44 +1,35 @@
 class PostsController < ApplicationController
   before_action :set_post, only: %i[ show edit update destroy ]
-
-  # GET /posts or /posts.json
   def index
     @posts = Post.all
   end
-
-  # GET /posts/1 or /posts/1.json
-  def show
-  end
-
-  # GET /posts/new
   def new
     @post = Post.new
   end
-
-  # GET /posts/1/edit
-  def edit
-  end
-
-  # POST /posts or /posts.json
   def create
     @post = Post.new(post_params)
-
-    respond_to do |format|
-      if @post.save
-        format.html { redirect_to @post, notice: "Post was successfully created." }
-        format.json { render :show, status: :created, location: @post }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @post.errors, status: :unprocessable_entity }
+    if params[:back]
+      render :new
+    else
+      respond_to do |format|
+        if @post.save
+          format.html { redirect_to posts_path, notice: "Post was successfully created." }
+          format.json { render :show, status: :created, location: @post }
+        else
+          format.html { render :new, status: :unprocessable_entity }
+          format.json { render json: @post.errors, status: :unprocessable_entity }
+        end
       end
     end
   end
-
-  # PATCH/PUT /posts/1 or /posts/1.json
+  def show
+  end
+  def edit
+  end
   def update
     respond_to do |format|
       if @post.update(post_params)
-        format.html { redirect_to @post, notice: "Post was successfully updated." }
+        format.html { redirect_to posts_path, notice: "Post was successfully updated." }
         format.json { render :show, status: :ok, location: @post }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -46,8 +37,6 @@ class PostsController < ApplicationController
       end
     end
   end
-
-  # DELETE /posts/1 or /posts/1.json
   def destroy
     @post.destroy
     respond_to do |format|
@@ -55,14 +44,14 @@ class PostsController < ApplicationController
       format.json { head :no_content }
     end
   end
-
+  def confirm
+    @post = Post.new(post_params)
+    render :new if @post.invalid?
+  end
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_post
       @post = Post.find(params[:id])
     end
-
-    # Only allow a list of trusted parameters through.
     def post_params
       params.require(:post).permit(:content)
     end
